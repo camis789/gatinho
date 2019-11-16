4f2bdc1bdf85414e6fcafe4d1c7b41c21d095702
Camila Caxambu <camila-caxambu@hotmail.com>
2019-11-16 03:56:19 -0300

    aaaaa.py

---
+++ aaaaa.py
@@ -0,0 +1,31 @@
+a = input ('Nossa olha só, que pedaço de mal caminho, fala teu nome aí safado')
+
+if a == 'Echter' or 'Henrique':
+    print ('olá gatinho, cola aqui lexk pra eu te falar uma parada')
+
+else:
+    print ('Try again')
+
+b = input ('Você me ama?')
+
+if b == 'sim' or 'Sim':
+    print ('então deixa eu continuar')
+
+else:
+    print ('para de me trollar demonio ')
+
+
+print ('Você é o amor da minha vida hihihi')
+print ('Isso aqui não é um slide mas olha como é coloridinhoo')
+print ('Eu amo você')
+print ('E o carinho que você tem por mim')
+print ('E como você é um amorzinho comigo')
+print ('aaa Eu espero que você leia meu caderninho as vezes')
+print (' e eu espero que você goste desse mimo aqui também')
+print ('Fiquei um tempão pesquisando p descobrir como q faz essa parada aqui em cima')
+print ('mas vale a pena')
+print ('porque é você')
+print ('e pra mim você sempre vai valer a pena hihi')
+print ('Obrigada por você ser você hihi')
+print ('E por sua rola encaixar em mim tão perfeitamente <3 ')
+print ('Mais uma vez: você é o amor da minha vida viu ')
